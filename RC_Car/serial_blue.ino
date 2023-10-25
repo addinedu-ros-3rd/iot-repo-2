2eddc00b8542faacf2a23ce7be9b8a3107fddafd
Han Seungjun <81555330+HanSeungJun@users.noreply.github.com>
2023-10-25 18:21:39 +0900

    Update serial_blue.ino
    add explain Serial.write and BTSerial

--- RC_Car/serial_blue.ino
+++ RC_Car/serial_blue.ino
@@ -12,9 +12,11 @@ void setup() {
 }
 
 void loop() {
+  // 블루투스쪽에서 데이터를 수신한 경우, 시리얼 모니터에  수신한 데이터를 출력한다.
   if (BTSerial.available()) {
     Serial.write(BTSerial.read());
   }
+  // 시리얼 모니터에서 데이터를 전송한 경우, 전송한 데이터를 블루투스 모듀을 통해 내보낸다
   if (Serial.available()) {
     BTSerial.write(Serial.read());
   }
