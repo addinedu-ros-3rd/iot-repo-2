29da862d8766b9e2e75a36aad796bc3aa9b7021f
YunOh21 <yun5.dev@gmail.com>
2023-10-25 18:16:11 +0900

    fix: case 구분

--- Conveyer_Belt/serial_db.py
+++ Conveyer_Belt/serial_db.py
@@ -31,20 +31,20 @@ while True:
             connected = True
     
     now_section = int(read.split(":")[0][-1]) + 1
+    uid = read.split(":")[-1].strip().upper()
+    now = datetime.now()
+    now_ts = now.strftime('%Y-%m-%d %H:%M:%S')
     
-    if now_section == 1:
-        query = "update rfid set in_time=%s, now_section=%s, section_update_time=%s where uid=%s"
-    else:
-        query = "update rfid set now_section=%s, section_update_time=%s where uid=%s"
-        
     try:
-        uid = read.split(":")[-1].strip().upper()
-        now = datetime.now()
-        now_ts = now.strftime('%Y-%m-%d %H:%M:%S')
-        
-        cursor.execute(query, (now_ts, now_section, now_ts, uid))
-        remote.commit()
-        # ser.write(b'1')  # 성공 알림
+        if now_section == 1:
+            query = "update rfid set in_time=%s, now_section=%s, section_update_time=%s where uid=%s"
+            cursor.execute(query, (now_ts, now_section, now_ts, uid))
+        else:
+            query = "update rfid set now_section=%s, section_update_time=%s where uid=%s"
+            cursor.execute(query, (now_section, now_ts, uid))
+                
+            remote.commit()
+            # ser.write(b'1')  # 성공 알림
     except Exception as e:
         print(e)
         # ser.write(b'0')  # 실패 알림
