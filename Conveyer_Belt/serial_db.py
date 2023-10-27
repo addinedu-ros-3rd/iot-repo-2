db71ee01d3bcf10cdefd2ea7a0f7d65872336f0e
YunOh21 <yun5.dev@gmail.com>
2023-10-27 18:14:55 +0900

    feat: in/store/out case add
    - 입고/보관/출고 케이스별 update 쿼리 수정

--- Conveyer_Belt/serial_db.py
+++ Conveyer_Belt/serial_db.py
@@ -18,48 +18,79 @@ remote = mysql.connector.connect(
 cursor = remote.cursor()
 ser = serial.Serial("/dev/ttyACM0", 9600)
 
-while True:
-    connected = False
 
-    while not connected:
+# UI에서 오분류를 감지하기 위해 serial read 모듈화
+def read_serial():
+    return ser.read().decode().strip()
+
+
+def update_in():
+    now_section = int(read.split(":")[0][-1]) + 1
+    
+    try:
+        query = "update rfid set in_time=%s, now_section=%s, section_update_time=%s where uid=%s"
+        cursor.execute(query, (now_ts, now_section, now_ts, uid))
+        remote.commit()
+        
+        query = "select category_id from rfid where uid=%s"
+        cursor.execute(query, (uid,))
+        category_id = cursor.fetchone()[0]
+        
+        ser.write(bytes(str(category_id), "utf-8"))  # 1, 2, 3
+            
+    except Exception as e:
+        print(e)
+        
+
+def update_store():
+    now_section = int(read.split(":")[1]) + 1
+    
+    try:
+        query = "update rfid set now_section=%s, section_update_time=%s where uid=%s"
+        cursor.execute(query, (now_section, now_ts, uid))
+        remote.commit()
+    except Exception as e:
+        print(e)
+        
+        
+def update_out():
+    now_section = 4
+    
+    try:
+        query = "update rfid set now_section=%s, section_update_time=%s, out_time=%s where uid=%s"
+        cursor.execute(query, (now_section, now_ts, now_ts, uid))
+        remote.commit()
+    except Exception as e:
+        print(e)
+
+
+while True:
+    inRead = False
+    storeRead = False
+    outRead = False
+
+    while (not inRead) and (not storeRead) and (not outRead):
         read = ser.readline().decode()
         print(read)
-        # print(len(read))
+        print(len(read))
+        # store:i:uid
+        # out:uid
         if len(read) == 33:
-            connected = True
+            inRead = True
     
-    now_section = int(read.split(":")[0][-1]) + 1
     uid = read.split(":")[-1].strip().upper()
     now = datetime.now()
     now_ts = now.strftime('%Y-%m-%d %H:%M:%S')  # 서버의 시간을 보관 = 서버 시간이 UTC이면 UTC로 보관(로컬 테스트 시 한국 시간 저장됨)
     
-    try:
-        if now_section == 1:  # 센터 최초 입고 시 인식
-            query = "update rfid set in_time=%s, now_section=%s, section_update_time=%s where uid=%s"
-            cursor.execute(query, (now_ts, now_section, now_ts, uid))
-            remote.commit()
+    # RFID를 읽는 경우와 아닌 경우(출고시각을 받는 경우)를 구분해야 함
+    if inRead == True:
+        update_in()
             
-            query = "select category_id from rfid where uid=%s"
-            cursor.execute(query, (uid,))
-            category_id = cursor.fetchone()[0]
-            
-            ser.write(bytes(str(category_id), "utf-8"))  # 1, 2, 3
-            
-        else:  # 각 창고 보관 시 인식
-            query = "update rfid set now_section=%s, section_update_time=%s where uid=%s"
-            cursor.execute(query, (now_section, now_ts, uid))
-            remote.commit()
-            
-            query = "select category_id from rfid where uid=%s"
-            cursor.execute(query, (uid,))
-            category_id = cursor.fetchone()[0]
-            
-            if now_section != (category_id + 1):
-                ser.write(b'w')  # 오분류 알림
-        # ser.write(b'1')  # 성공 알림
-    except Exception as e:
-        print(e)
-        ser.write(b'0')  # 실패 알림
+    elif storeRead == True:
+        update_store()
+        
+    else:
+        update_out()
 
 ser.close()
 cursor.close()
